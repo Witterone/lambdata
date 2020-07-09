1b51447ccd90c8ffe19e216ebb2cc1608596cf0c
Ava Wingfield <bwitter770@gmail.com>
2020-07-09 16:59:10 -0400

    added files as recommended by James

---
+++ lambdata_witterone/practice_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Create Unittests for both modules
+# Make sure each class/method includes a docstring
+# Make sure entire script conforms to PEP8 guidelines
+# To test, run the script in the terminal.
+
+
+# Standard Python Library
+import unittest
+
+# Local Imports
+from sprint_review import Puppy, Leech
+from exercise import SimpleOperations, Complex
+
+
+case1 = SimpleOperations(3, 2)
+case2 = Complex(3, 2)
+case3 = Puppy('James', 5)
+case4 = Leech('Jonathan', 10, True)
+
+
+class ArithmeticTest(unittest.TestCase):
+    # TODO
+
+
+class PuppyTest(unittest.TestCase):
+    # TODO
+
+
+if __name__ == "__main__":
+    unittest.main()
